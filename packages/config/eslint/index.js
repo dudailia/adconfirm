110bb1233af8973c42d21f8e320fb95271734154
ilia duda <i.i.duda@icloud.com>
2026-05-12 22:31:48 +0200

    fix: correct tsconfig/node moduleResolution and ESLint peer deps

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -5,6 +5,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   rules: {
+    "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-explicit-any": "warn",
   },
